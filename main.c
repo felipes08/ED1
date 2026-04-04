59f226cf81e7e9977ed7d0bc078a9e7514e951e1
Felipe Silva de Almeida <felipe.salmeida.008@gmail.com>
2026-04-03 23:29:52 -0300

    terminei os exercícios (aula27_03)

--- main.c
+++ main.c
@@ -236,7 +236,7 @@ void Linked_copia_inverte(LinkedList *L1, LinkedList *L2)
 
 
 
-LinkedList *LinkedList_append(LinkedList *L1, LinkedList *L2)
+void LinkedList_append(LinkedList *L1, LinkedList *L2)
 {
     Node *p = L2->begin;
 
@@ -246,70 +246,73 @@ LinkedList *LinkedList_append(LinkedList *L1, LinkedList *L2)
         LinkedList_add_last(L1, p->val);
         p = p->next;
     }
-    return L1;
 
 }
 
 
 
-LinkedList *LinkedList_concatena(LinkedList *L1, LinkedList *L2)
+void LinkedList_concatena(LinkedList *L1, LinkedList *L2, LinkedList *L3)
 {
-    LinkedList *L3 = LinkedList_create();
     Linked_copia(L1, L3);
     LinkedList_append(L3, L2);
-    return L3;
 }
 
 void LinkedList_transforma(LinkedList *L1, LinkedList *L2, LinkedList *L3)
-
 {
-
-    if(L1->begin != NULL)
+    if(L1->begin == NULL)
     {
-
+        L3->begin = L2->begin;
+        L3->end = L2->end;
+        L3->end->next = NULL;
+        L3->contador = L2->contador;
     }
-    L3->begin = L1->begin;
-    L1->end->next = L2->begin;
-    L3->end = L2->end;
-    L3->end->next = NULL;
-    L3->contador = L1->contador + L2->contador;
+    else if(L2->begin == NULL)
+    {
+        L3->begin = L1->begin;
+        L3->end = L1->end;
+        L3->end->next = NULL;
+        L3->contador = L2->contador;
+    }
+    else
+    {
+        L3->begin = L1->begin;
+        L1->end->next = L2->begin;
+        L3->end = L2->end;
+        L3->end->next = NULL;
+        L3->contador = L1->contador + L2->contador;
+    }
     free(L1);
     free(L2);
 
 }
 
-//void LinkedList_transforma(LinkedList *L1, LinkedList *L2, LinkedList *L3)
-//{
-//    // 1. Verificação de segurança: se L1 está vazia
-//    if (L1->begin == NULL) {
-//        L3->begin = L2->begin;
-//        L3->end = L2->end;
-//        L3->contador = L2->contador;
-//    }
-//    // 2. Se L2 está vazia
-//    else if (L2->begin == NULL) {
-//        L3->begin = L1->begin;
-//        L3->end = L1->end;
-//        L3->contador = L1->contador;
-//    }
-//    // 3. Ambas têm elementos: Conecta o fim de uma no início da outra
-//    else {
-//        L3->begin = L1->begin;
-//        L1->end->next = L2->begin;
-//        L3->end = L2->end;
-//        L3->end->next = NULL;
-//        L3->contador = L1->contador + L2->contador;
-//    }
-//
-//    // 4. Libera a estrutura de controle (as "caixas"), mas não os nós!
-//    free(L1);
-//    free(L2);
-//
-//    // IMPORTANTE: Na main, você deve fazer La = NULL; Lb = NULL;
-//    // após chamar essa função para evitar acidentes.
-//}
+void LinkedList_merge(LinkedList *L1, LinkedList *L2, LinkedList *L3)
+{
+    Node *p1 = L1->begin;
+    Node *p2 = L2->begin;
+    bool flag = true;
 
+    while(p1 != NULL && p2 != NULL){
+        if (flag){
+            LinkedList_add_last(L3, p1->val);
+            p1 = p1->next;
+        } else{
+            LinkedList_add_last(L3, p2->val);
+            p2 = p2->next;
+        }
+        flag = !flag;
+    }
 
+    while(p1 != NULL){
+        LinkedList_add_last(L3, p1->val);
+        p1 = p1->next;
+    }
+
+    while(p2 != NULL){
+        LinkedList_add_last(L3, p2->val);
+        p2 = p2->next;
+    }
+}
 
 
 int main (){
@@ -346,14 +349,14 @@ int main (){
     printf("Numeros de elementos: %d\n", LinkedList_numero_elem(La));
     LinkedList_print(La);
 
-    ////Removendo 4 elementos da lista(A)
-    printf("\nRemovendo 4 elementos da Lista A:\n");
-    LinkedList_rem(La, 10);
-    LinkedList_rem(La, 2);
-    LinkedList_rem(La, 4);
-    LinkedList_rem(La, 5);
-    LinkedList_print(La);
-    printf("Numeros de elementos: %d\n\n", LinkedList_numero_elem(La));
+      //Removendo 4 elementos da lista(A)
+//    printf("\nRemovendo 4 elementos da Lista A:\n");
+//    LinkedList_rem(La, 10);
+//    LinkedList_rem(La, 2);
+//    LinkedList_rem(La, 4);
+//    LinkedList_rem(La, 5);
+//    LinkedList_print(La);
+//    printf("Numeros de elementos: %d\n\n", LinkedList_numero_elem(La));
 
 
 
@@ -364,50 +367,68 @@ int main (){
     printf("Numeros de elementos: %d\n\n", LinkedList_numero_elem(La));
 
 
+    printf("Lista A: ");
+    LinkedList_print(La);
     //adicionando elementos no final da lista(B)
-    printf("Lista B:\n");
+    printf("Lista B: ");
     LinkedList_add_last(Lb, 5);
     LinkedList_add_last(Lb, 15);
     LinkedList_add_last(Lb, 50);
     LinkedList_print(Lb);
-
-    //adicionando a lista(B) no final da lista(A)
-    printf("\nLinkedList_append(La, Lb):\n");
-    LinkedList_append(La, Lb);
-    LinkedList_print(La);
-
-    printf("\nLinked_copia_inverte(La, Lc);\n");
-    Linked_copia_inverte(La, Lc);
+    printf("Lista C: ");
     LinkedList_print(Lc);
 
-
-    //imprime as Lista A e a Lista B na tela
-    printf("\nLista A: ");
-    LinkedList_print(La);
-    printf("Lista B: ");
-    LinkedList_print(Lb);
-    LinkedList_transforma(La, Lb, Lc);
-    La = NULL;
-    Lb = NULL;
-
-    printf("\nLista C: ");
-    LinkedList_print(Lc);
-    printf("\nLista A: ");
-    LinkedList_print(La);
-    printf("Lista B: ");
-    LinkedList_print(Lb);
-
-
-//    printf("\nLinkedList_concatena(La,Lb);\n");
-//    Lc = LinkedList_concatena(La,Lb);
+        //adicionando a lista(B) no final da lista(A)
+//    printf("\nLinkedList_append(La, Lb):\n");
+//    LinkedList_append(La, Lb);
+//    LinkedList_print(La);
+//
+//    printf("\nLinked_copia_inverte(La, Lc);\n");
+//    Linked_copia_inverte(La, Lc);
 //    LinkedList_print(Lc);
 //
 //
-//    //imprime as Lista A e a Lista B na tela
+        //imprime as Lista A e a Lista B na tela
 //    printf("\nLista A: ");
 //    LinkedList_print(La);
 //    printf("Lista B: ");
 //    LinkedList_print(Lb);
+//    printf("Lista C: ");
+//    LinkedList_print(Lc);
+//
+        //trasforma a Lista A e Lista B em uma Lista C mantendo os endereços de memória originais
+//    LinkedList_transforma(La, Lb, Lc);
+//    La = NULL;
+//    Lb = NULL;
+//
+        //imprime as 3 listas
+//    printf("\nLista C: ");
+//    LinkedList_print(Lc);
+//    printf("\nLista A: ");
+//    LinkedList_print(La);
+//    printf("Lista B: ");
+//    LinkedList_print(Lb);
+//    Lc = NULL;
+//    printf("a");
+
+
+//    printf("\n\n\nLinkedList_concatena(La,Lb);\nLista C: ");
+//    LinkedList_concatena(La, Lb, Lc);
+//    LinkedList_print(Lc);
+
+
+        //imprime as Lista A e a Lista B na tela
+//    printf("\nLista A: ");
+//    LinkedList_print(La);
+//    printf("Lista B: ");
+//    LinkedList_print(Lb);
+
+
+    LinkedList_merge(La, Lb, Lc);
+    printf("Lista C: ");
+    LinkedList_print(Lc);
+
+
 
 //    printf("elemento 1 = %d\n", LinkedList_PrintBegin(La));
 //    printf("elemento %d = %d\n", La->contador, LinkedList_PrintEnd(La));
